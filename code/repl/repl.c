2c07f19cfb48c5171e60fcbda12a4c16f6c5aafe
Mike Mallon <mikemallon99@gmail.com>
2023-03-22 18:51:30 -0500

    got a working lexer repl

--- code/repl/repl.c
+++ code/repl/repl.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 #include "../lexer/lexer.c"
 
 void start() {
-    lexer l;
     char input[100];
     while (true) {
         printf(">> ");
         fgets(input, 100, stdin);
+
+        if (strcmp(input, "") == 0) {
+            return;
+        }
+
+        lexer l = get_lexer(input);
+        for (token t = next_token(&l); t.tokenType != EOF_T; t = next_token(&l)) {
+            char* type_string = get_token_type_string(t.tokenType);
+            printf("{Type: %s, Value: '%s'}\n", type_string, t.value);
+            free(type_string);
+            free(t.value);
+        }
     }
+
 }
 
--- code/tokens.c
+++ code/tokens.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef enum token_type {
@@ -64,4 +65,93 @@ token_type lookup_ident(char* ident) {
     } else {
         return IDENT;
     }
+}
+
+char* get_token_type_string(token_type t) {
+    char* token_string = calloc(10, 1);
+
+    switch (t) {
+        case ILLEGAL:
+            strcpy(token_string, "ILLEGAL");
+            break;
+        case EOF_T:
+            strcpy(token_string, "EOF_T");
+            break;
+        case IDENT:
+            strcpy(token_string, "IDENT");
+            break;
+        case INT:
+            strcpy(token_string, "INT");
+            break;
+        case ASSIGN:
+            strcpy(token_string, "ASSIGN");
+            break;
+        case PLUS:
+            strcpy(token_string, "PLUS");
+            break;
+        case MINUS:
+            strcpy(token_string, "MINUS");
+            break;
+        case BANG:
+            strcpy(token_string, "BANG");
+            break;
+        case ASTERISK:
+            strcpy(token_string, "ASTERISK");
+            break;
+        case SLASH:
+            strcpy(token_string, "SLASH");
+            break;
+        case LT:
+            strcpy(token_string, "LT");
+            break;
+        case GT:
+            strcpy(token_string, "GT");
+            break;
+        case EQ:
+            strcpy(token_string, "EQ");
+            break;
+        case NOT_EQ:
+            strcpy(token_string, "NOT_EQ");
+            break;
+        case COMMA:
+            strcpy(token_string, "COMMA");
+            break;
+        case SEMICOLON:
+            strcpy(token_string, "SEMICOLON");
+            break;
+        case LPAREN:
+            strcpy(token_string, "LPAREN");
+            break;
+        case RPAREN:
+            strcpy(token_string, "RPAREN");
+            break;
+        case LBRACE:
+            strcpy(token_string, "LBRACE");
+            break;
+        case RBRACE:
+            strcpy(token_string, "RBRACE");
+            break;
+        case FUNCTION:
+            strcpy(token_string, "FUNCTION");
+            break;
+        case LET:
+            strcpy(token_string, "LET");
+            break;
+        case TRUE:
+            strcpy(token_string, "TRUE");
+            break;
+        case FALSE:
+            strcpy(token_string, "FALSE");
+            break;
+        case IF:
+            strcpy(token_string, "IF");
+            break;
+        case ELSE:
+            strcpy(token_string, "ELSE");
+            break;
+        case RETURN:
+            strcpy(token_string, "RETURN");
+            break;
+    }
+    return token_string;
 }
